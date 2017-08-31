e01ad643b6f2f50f4d1ba2c8efbf8af0ee16b763
khbyun <novemberde@nate.com>
2017-08-31 03:28:49 +0000

    test child_process

---
+++ go/README.md
@@ -0,0 +1,15 @@
+# 개요
+
+---
+
+##병행처리
+
+- 통신 순차 프로세스 Communicating Sequential Processes, CSP
+- 메시지를 전달하는 방식으로 동기화
+    - 고루틴 -> 채널 -> 고루틴2 -> 채널 -> 고루틴3
+
+---
+
+## Static + Dynamic
+
+- 
---
+++ javascript/child_process.js
@@ -0,0 +1,25 @@
+const process = require("child_process");
+
+// console.log(process);
+
+// process.exec("ls", (a, b) => {
+//     console.log(a);
+//     console.log(b);
+// })
+
+const exec = command => {
+  return new Promise((resolve, reject) => {
+    process.exec(command, (err, stdout, stderr) => {
+      if(err) return reject(stderr);
+      return resolve(stdout);
+    });
+  });
+}
+
+exec("ls")
+.then( result => console.log(result))
+.catch( err => console.error(err));
+
+exec("lsasdfad")
+.then( result => console.log(result))
+.catch( err => console.error(err));
