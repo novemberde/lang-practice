b380695a1af789cb71d81f003c5378ff6c1c754e
kyuhyun <novemberde1@gmail.com>
2017-09-26 13:38:04 +0900

    add StringBufferTest

---
+++ java/StringBufferTest.java
@@ -0,0 +1,35 @@
+/**
+ * StringBufferTest
+ */
+public class StringBufferTest {
+
+  public static void main(String[] args) {
+    StringBuffer stringBuffer = new StringBuffer();
+    StringBuilder stringBuilder = new StringBuilder();
+    
+    new Thread(() -> {
+        for(int i=0; i<10000; i++) {
+            stringBuffer.append(i);
+            stringBuilder.append(i);
+        }
+    }).start();
+    
+    new Thread(() -> {
+        for(int i=0; i<10000; i++) {
+            stringBuffer.append(i);
+            stringBuilder.append(i);
+        }
+    }).start();
+    
+    new Thread(() -> {
+        try {
+            Thread.sleep(5000);
+    
+            System.out.println("StringBuffer.length: "+ stringBuffer.length());
+            System.out.println("StringBuilder.length: "+ stringBuilder.length());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }).start();
+  }
+}
